a3f2cfd46633a3df012ce6963c05bc2b741b4196
J.J. Montgomery <jmonty42.dev@gmail.com>
2024-12-10 23:17:08 -0800

    WIP: Day 11 Part 2
    Naively running through the problem for 75 iterations will not work if
    you want the answer in this geological era. This is my first attempt to
    optomize a solution, but it's not performing well yet and I don't have
    confidence that I'd get the solution in my lifetime as it runs now.
    
    But the basic approach would be to save the evolution of each stone in
    case you encounter stones with the same values, you can re-use previous
    work.

--- 2024/11/solution.py
+++ 2024/11/solution.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from typing import List
+from typing import List, Dict
 
-def blink(stones: [int]) -> List[int]:
-    new_stones_list = []
-    for stone in stones:
+def blink(stone: int, iterations: Dict[int, List[List[int]]]) -> List[int]:
+    if stone not in iterations:
+        new_stones_list = []
+
         if stone == 0:
             new_stones_list.append(1)
         elif len(str(stone)) % 2 == 0:
@@ -12,12 +13,34 @@ def blink(stones: [int]) -> List[int]:
             new_stones_list.append(int(str(stone)[mid:]))
         else:
             new_stones_list.append(stone*2024)
-    return new_stones_list
 
-def blink_25(stones: [int]) -> int:
-    for _ in range(25):
-        stones = blink(stones)
-    return len(stones)
+        iterations[stone] = [new_stones_list]
+
+    return iterations[stone][0]
+
+def continue_iterating_on_stone(stone: int, n: int, iterations: Dict[int, List[List[int]]]):
+    for iteration in range(len(iterations[stone]), n):
+        this_iteration_stone_list = []
+        for old_stone in iterations[stone][iteration - 1]:
+            blink_n_times(old_stone, n - iteration, iterations)
+            this_iteration_stone_list += iterations[old_stone][0]
+        if iteration == len(iterations[stone]):
+            iterations[stone].append(this_iteration_stone_list)
+
+def blink_n_times(stone: int, n: int, iterations: Dict[int, List[List[int]]]) -> int:
+    if stone not in iterations:
+        iterations[stone] = [blink(stone, iterations)]
+    if len(iterations[stone]) < n:
+        continue_iterating_on_stone(stone, n, iterations)
+
+    return len(iterations[stone][n-1])
+
+def blink_n(stones: [int], n: int) -> int:
+    count = 0
+    iterations = {} # starting value -> [] result after n+1 iterations
+    for stone in stones:
+        count += blink_n_times(stone, n, iterations)
+    return count
 
 if __name__=="__main__":
     input_file = open("input.txt", 'r')
@@ -25,6 +48,11 @@ if __name__=="__main__":
     input_file.close()
 
     before = datetime.now()
-    stone_count = blink_25(stones)
+    stone_count = blink_n(stones, 25)
     after = datetime.now()
     print("Part 1: {} ({})".format(stone_count, after - before))
+
+    before = datetime.now()
+    stone_count = blink_n(stones, 75)
+    after = datetime.now()
+    print("Part 2: {} ({})".format(stone_count, after - before))
